417cd93bbe4023e3fa645dc40851083c3b30bc14
Lucas Sousa Silva <luuhsousa2012@gmail.com>
2023-02-08 17:02:44 -0300

    testes da checkout two

---
+++ cypress/support/pages/checkoutStepTwo/elements.js
@@ -0,0 +1,8 @@
+exports.el = {
+    title : '.title',
+   paymentInformantion: '.summary_info > :nth-child(2)',
+   itemTotalPrice: '.summary_subtotal_label',
+   taxPrice: '.summary_tax_label',
+   totalResultPrice: '.summary_total_label'
+};
+  
